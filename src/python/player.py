9fb3f6a6bc820e33e34eae7e0ed11aa05956e40b
Pezh <Pezh@reshut1-58-51-dhcp.int.colorado.edu>
2015-10-12 17:36:52 -0600

    add the camera function with the image of mario

--- src/python/player.py
+++ src/python/player.py
@@ -6,6 +6,8 @@
 # DateCreated:2015/10/11/  
 #
 from pygame import *
+import pyganim
+import os 
 gPlayer = 0
 gPlayerMoveSpeed = 7
 gPlayerWidth = 22
@@ -14,6 +16,24 @@ gPlayerDisplay = (gPlayerWidth,gPlayerHeight)
 gPlayerColor = "#888888"
 gPlayerJumpPower = 8
 gEnvGravity = 0.35
+
+ICON_DIR = os.path.dirname (__file__) # full path to the directory with files
+ANIMATION_DELAY = 0.1
+ANIMATION_RIGHT = [('%s/mario/r1.png'% ICON_DIR), 
+            ('%s/mario/r2.png'% ICON_DIR), 
+            ('%s/mario/r3.png'% ICON_DIR), 
+            ('%s/mario/r4.png'% ICON_DIR), 
+            ('%s/mario/r5.png'% ICON_DIR)] 
+ANIMATION_LEFT = [('%s/mario/l1.png'% ICON_DIR), 
+            ('%s/mario/l2.png'% ICON_DIR), 
+            ('%s/mario/l3.png'% ICON_DIR), 
+            ('%s/mario/l4.png'% ICON_DIR), 
+            ('%s/mario/l5.png'% ICON_DIR)] 
+ANIMATION_JUMP_LEFT = [('%s/mario/jl.png'% ICON_DIR, 0.1)] 
+ANIMATION_JUMP_RIGHT = [('%s/mario/jr.png'% ICON_DIR, 0.1)] 
+ANIMATION_JUMP = [('%s/mario/j.png'% ICON_DIR, 0.1)] 
+ANIMATION_STAY = [('%s/mario/0.png'% ICON_DIR, 0.1)] 
+
 class Player(sprite.Sprite):
     def __init__(self,x,y):
         sprite.Sprite.__init__(self)
@@ -25,17 +45,54 @@ class Player(sprite.Sprite):
         self.image = Surface((gPlayerDisplay))
         self.image.fill(Color(gPlayerColor))
         self.rect = Rect(x, y, gPlayerWidth, gPlayerHeight)
+        self.image.set_colorkey(Color(gPlayerColor))
+        boltAnim = []
+        for anim in ANIMATION_RIGHT:
+            boltAnim.append ((anim, ANIMATION_DELAY))
+        self.boltAnimRight = pyganim.PygAnimation (boltAnim)
+        self.boltAnimRight.play ()
+        boltAnim = []
+        for anim in ANIMATION_LEFT:
+            boltAnim.append((anim, ANIMATION_DELAY))
+        self.boltAnimLeft = pyganim.PygAnimation(boltAnim)
+        self.boltAnimLeft.play()
+        self.boltAnimStay = pyganim.PygAnimation (ANIMATION_STAY)
+        self.boltAnimStay.play ()
+        self.boltAnimStay.blit (self.image, (0, 0)) # By default, the stand
         
+        self.boltAnimJumpLeft = pyganim.PygAnimation (ANIMATION_JUMP_LEFT)
+        self.boltAnimJumpLeft.play ()
+        
+        self.boltAnimJumpRight = pyganim.PygAnimation (ANIMATION_JUMP_RIGHT)
+        self.boltAnimJumpRight.play ()
+        
+        self.boltAnimJump = pyganim.PygAnimation (ANIMATION_JUMP)
+        self.boltAnimJump.play ()
     def update(self, left, right, up, platforms):
         if up:
             if self.onGround:
                 self.yvel = -gPlayerJumpPower
+            self.image.fill(Color(gPlayerColor))
+            self.boltAnimJump.blit(self.image, (0,0))
         if left:
             self.xvel = -gPlayerMoveSpeed
+            self.image.fill(Color(gPlayerColor))
+            if up:
+                self.boltAnimJumpLeft.blit(self.image, (0,0))
+            else:
+                self.boltAnimLeft.blit(self.image, (0,0))
         if right:
             self.xvel = gPlayerMoveSpeed
+            self.image.fill(Color(gPlayerColor))
+            if up:
+                self.boltAnimJumpRight.blit(self.image, (0,0))
+            else:
+                self.boltAnimRight.blit(self.image, (0,0))
         if not (left or right):
             self.xvel = 0
+            if not up:
+                self.image.fill(Color(gPlayerColor))
+                self.boltAnimStay.blit(self.image,(0,0))
         if not self.onGround:
             self.yvel = self.yvel + gEnvGravity
         self.onGround = False
