2be8e9da8920b1f9e5fd0575c6b0d7431f98b1ea
Elias Ortiz <elias.ortiz@colorado.edu>
2015-11-11 19:12:21 -0700

    Added in php file that works correctly with sql server

--- leaderboard.php
+++ leaderboard.php
@@ -3,7 +3,11 @@
 <head>
 <style>
 table, th, td {
-     border: 1px solid black;
+     border: 2px solid black;
+     font-family:sans-serif;
+     vertical-align:center;
+     text-align:center;
+     font-size:20pt;
 }
 </style>
 </head>
@@ -12,7 +16,7 @@ table, th, td {
 <?php
 $servername = "45.55.26.125:3306";
 $username = "HTML";
-$password = "";
+$password = "website3308!";
 $dbname = "Game_Data";
 
 // Create connection
@@ -22,14 +26,15 @@ if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id FROM Scores";
+$sql = "SELECT DISTINCT userName, scores FROM Scores ORDER BY scores DESC";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-     echo "<table><tr><th>ID</th><th>Score</th></tr>";
+     echo "<table align='center' width='40%' cellpadding='3' cellspacing='3'><tr><th colspan='2'><h2><br>The Big Bad Game</h3></th></tr>";
+     echo "<tr><th>Player Name</th><th>Score</th></tr>";
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         echo "<tr><td>" . $row["id"]. "</td><td>" . $row["firstname"]. " " . $row["lastname"]. "</td></tr>";
+         echo "<tr><td>" . $row["userName"]. "</td><td>" . $row["scores"]. "</td></tr>";
      }
      echo "</table>";
 } else {
