36160f32d1abc84e672dd6382b0ae6ad1092d674
Elias Ortiz <elias.ortiz@colorado.edu>
2015-12-06 13:58:11 -0700

    I have made a very early use of tabs for the website (not finished)

---
+++ home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+     background-color:#565A5C;
+     font-family:arial;
+}
+p{
+    color:white;
+    font-size:20pt;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 1px solid #e7e7e7;
+    background-color: #A2A4A3;
+    font-family:arial;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    color:white;
+    background-color: #3f3f3f;
+}
+
+li a.active {
+    color: black;
+    background-color: #CFB87C;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="home.php">Home</a></li>
+  <li><a href="leaderboard.php">Leaderboard</a></li>
+  <li><a href="doc.php">Other</a></li>
+</ul>
+<p>Welcome to the home of the Big Bad Game!</p>
+</body>
+</html>
--- leaderboard.php
+++ leaderboard.php
@@ -15,10 +15,46 @@ table, th, td {
 body {
      background-color:#565A5C;
 }
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 1px solid #e7e7e7;
+    background-color: #A2A4A3;
+    font-family:arial;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    color:white;
+    background-color: #3f3f3f;
+}
+
+li a.active {
+    color: black;
+    background-color: #CFB87C;
+}
 </style>
 </head>
 <body>
 
+<ul>
+  <li><a href="home.php">Home</a></li>
+  <li><a class="active" href="leaderboard.php">Leaderboard</a></li>
+  <li><a href="doc.php">Other</a></li>
+</ul>
 <?php
 $servername = "45.55.26.125:3306";
 $username = "HTML";
@@ -32,7 +68,7 @@ if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT DISTINCT userName, scores FROM Scores ORDER BY scores DESC";
+$sql = "SELECT DISTINCT userName, scores FROM Scores ORDER BY scores";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
